204ffe1024af2645e1f32d3a2cde500f5dfbcbd6
Samantha-Ruth <sam.haberman@gmail.com>
2022-11-01 08:17:20 -0600

    setup connection to database in flask, creating g object

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from app.routes import home
 from app.routes import home, dashboard
 from flask import Flask
+from app.db import init_db
 
 def create_app(test_config=None):
   # set up app config
@@ -16,6 +17,8 @@ def create_app(test_config=None):
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+
+  init_db(app)
 
   return app  
 
